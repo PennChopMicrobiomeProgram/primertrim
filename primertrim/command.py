40f270b6870e1df423be7373d301c039b0846111
Kyle Bittinger <kylebittinger@gmail.com>
2021-05-20 07:07:41 -0400

    Add sequence length to log file

--- primertrim/command.py
+++ primertrim/command.py
@@ -100,7 +100,9 @@ def main(argv=None):
     if args.log:
         for read_id, matchobj in trimmable_reads.matches.items():
             if matchobj is None:
-                args.log.write("{0}\t\t\t\t\n".format(read_id))
+                seq = trimmable_reads.seqs[read_id]
+                args.log.write("{0}\tNo match\t{1}\t\t\n".format(
+                    read_id, len(seq)))
             else:
                 args.log.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(
                     read_id, matchobj.method, matchobj.start,
