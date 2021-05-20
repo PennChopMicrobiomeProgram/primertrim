73b2eac6540c035bd3ec8a462cb1aa39fc86f3b5
Kyle Bittinger <kylebittinger@gmail.com>
2021-05-20 16:50:23 -0400

    Add tests for TrimmableReads

---
+++ tests/test_fastq.py
@@ -0,0 +1,56 @@
+from primertrim.fastq import TrimmableReads
+
+from collections import namedtuple
+
+
+input_reads = [
+    ("seq1", "ATGTCATGACTTGACTGCGG", "FFFFFFFFFFFFFFFFFFFF"),
+    ("seq2", "AGTCACGCTGACTGCATTGA", "FFFFFFFFFFFFFFFFFFFF"),
+    ("seq3", "TACGTCATGCATCGTAGTAA", "FFFFFFFFFFFFFFFFFFFF"),
+]
+
+output_reads = [
+    ("seq1", "ATGTCATGACTTGACTGCGG", "FFFFFFFFFFFFFFFFFFFF"),
+    ("seq2", "AGTCACGCTG", "FFFFFFFFFF"),
+    ("seq3", "TACGTCATGCATCGTAGTAA", "FFFFFFFFFFFFFFFFFFFF"),
+]
+
+output_loginfo = [
+    ("seq1", "", "", "", ""),
+    ("seq2", "Complete", 10, 0, "ACTGCATTGA"),
+    ("seq3", "", "", "", ""),
+]
+
+class MockMatch:
+    method = "Complete"
+    start = 10
+    mismatches = 0
+    primerseq = "ACTGCATTGA"
+
+
+def test_register_match():
+    t = TrimmableReads(input_reads)
+
+    read_ids = [read_id for read_id, seq in t.get_unmatched_seqs()]
+    assert read_ids == ["seq1", "seq2", "seq3"]
+
+    m = MockMatch()
+    t.register_match("seq2", m)
+
+    read_ids = [read_id for read_id, seq in t.get_unmatched_seqs()]
+    assert read_ids == ["seq1", "seq3"]
+
+
+def test_output():
+    t = TrimmableReads(input_reads)
+
+    # No matches found, nothing changed from input
+    assert list(t.output_reads()) == input_reads
+
+    m = MockMatch()
+    t.register_match("seq2", m)
+
+    # Now we have the expected output
+    assert list(t.output_reads()) == output_reads
+    assert list(t.output_loginfo()) == output_loginfo
+
